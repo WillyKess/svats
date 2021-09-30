8c639627aac9177eceb7b6670e80169028ca72cb
boiimakillu <ilikenuts203@gmail.com>
2021-09-29 21:04:40 -0400

    Proper base, with all four technologies working (hopefully) and ready for dev

---
+++ src/lib/Intro.svelte
@@ -0,0 +1,20 @@
+<script type="text/typescript">
+    import { Button } from 'attractions';
+</script>
+
+<main>
+    <div class="title">
+        <h1>
+            Hello!
+        </h1>
+        <Button>Wee</Button>
+    </div>
+</main>
+
+<style>
+    .title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
